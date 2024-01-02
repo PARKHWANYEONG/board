3be584a72246a1ef8cf98b806363f231390cfb58
PARKHWANYEONG <welcome0828@naver.com>
2024-01-03 06:35:24 +0900

    feat: 회원가입 API 기능 js파일 추가

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,33 @@
+const main = {
+    init : function(){
+
+        // 회원 가입
+        $('#btn-save').on('click', ()=>{
+            this.save();
+        });
+    },
+
+    save : function(){
+        const data = {
+            username: $('#username').val(),
+            password: $('#password').val(),
+            email: $('#email').val(),
+            nickname: $('#nickname').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/user',
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'JSON'
+        }).done(function(res){
+            alert('가입 완료.');
+            location.href= '/';
+        }).fail(function(err){
+            alert(JSON.stringify(err));
+        });
+    }
+}
+
+main.init();
